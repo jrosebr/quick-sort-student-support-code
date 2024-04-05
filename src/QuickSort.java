35ef2ce19ba3197cff7268be56f76f926c8dc27c
jrosebr <jrosebr@iu.edu>
2024-04-05 09:01:22 -0400

    update

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -7,8 +7,35 @@ public class QuickSort {
      * @param <E>   The element type for the sequence.
      */
     public static <E extends Comparable<? super E>>
-    void quicksort(Iterator<E> begin, Iterator<E> end) {
-        return;  // replace this line with your code
+    void quicksort(Iterator<E> begin, Iterator<E> end)
+    {
+        if (begin.equals(end))
+            return;
+
+        Iterator<E> pivot_position = partition(begin, end);
+
+        quicksort(begin, pivot_position);
+        pivot_position.advance();
+        quicksort(pivot_position, end);
+    }
+
+    public static <E extends Comparable<? super E>>
+    Iterator<E> partition(Iterator<E> begin, Iterator<E> end)
+    {
+        Iterator<E> pivot = Algorithms.last(begin, end);
+        Iterator<E> i = begin.clone();
+        Iterator<E> stop = Algorithms.last(begin, end);
+
+        for (Iterator<E> j = begin.clone(); !j.equals(stop); j.advance())
+        {
+            if ((j.get()).compareTo(pivot.get()) <= 0) {
+                Algorithms.iter_swap(i, j);
+                i.advance();
+            }
+        }
+
+        Algorithms.iter_swap(i, pivot);
+        return i;
     }
 
 }
--- test/StudentTest.java
+++ test/StudentTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 //import sequences.*;
 import java.util.Arrays;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 //import static org.junit.Assert.assertEquals;
@@ -9,8 +10,31 @@ import static org.junit.jupiter.api.Assertions.*;
 public class StudentTest {
 
     @Test
+    public void randomTest()
+    {
+        Random r = new Random();
+        LinkedList<Integer> test = new LinkedList<>();
+        Integer[] oracle = new Integer[20];
+
+        for (int i = 0; i != 20; ++i)
+        {
+            int x = r.nextInt();
+            oracle[i] = x;
+
+            test.insert_front(x);
+        }
+
+        QuickSort.quicksort(test.begin(), test.end());
+        Arrays.sort(oracle);
+        ArraySequence<Integer> oracle1 = new ArraySequence<>(oracle);
+
+        assertTrue(Algorithms.equal_sequences(oracle1, test));
+    }
+
+
+    @Test
     public void test() {
-        // your tests go here
+        randomTest();
     }
 
 }
